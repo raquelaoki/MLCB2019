639f7ab5333e59c387aba29a8cfc574422469e59
Raquel Aoki <23690490+raquelaoki@users.noreply.github.com>
2019-02-26 15:10:31 -0800

    v001

--- script_v002.R
+++ script_v002.R
@@ -1,133 +1,77 @@
 rm(list=ls())
 #-----#-----#-----#-----#-----#-----#-----#-----#-----#-----#-----#
 #AUTHOR: Raquel Aoki
-#DATE: 2019/02/08
-#Explorative cbioportal
-
+#DATE: 2019/02/26
 #-----#-----#-----#-----#-----#-----#-----#-----#-----#-----#-----#
 #Workdiretory 
-setwd("~/GitHub/project_spring2019")
+setwd("C:\\Users\\raoki\\Documents\\GitHub\\project_spring2019")
+
+#Exploring the clinical data
+#- Table S1. Detailed Information about TCGA-CDR and Analysis Results, 
+#Related to Figures 1, 2, and S1-S5 and Tables 1-5 and S2-S4. TCGA-CDR 
+#contains processed data for each individual cancer case including 
+#essential clinicopathologic data and the four clinical outcome 
+#endpoints of PFI, OS, DFI, and DSS. TCGA-CDR_Notes provides descriptions 
+# of the data fields and the recommended use of the data fields. 
+#- ExtraEndpoints contains additional clinical outcome endpoints. 
+#- ExtraEndpoints_Notes provides descriptions of the data elements in Tab ExtraEndpoints. 
+#- Table4_PHAssumptionTests shows the results of Cox PHs assumption tests 
+#for models in Table 4, with notes. Table5_PHAssumptionTests shows
+# the results of Cox PHs assumption tests for models in Table 5, with notes. 
+#- TSS_Info shows information of TCGA tissue source sites. 
+#Figure 2EFG_AdditionalInfo shows the results of additional analyses 
+# of the example reported in Figures 2E-2G, including Cox PHs assumption 
+#tests and competing risk assessment.
 
 
-if (!require("FirebrowseR")) devtools::install_github("mariodeng/FirebrowseR")
-if (!require("ggplot2")) install.packages("ggplot2")
+if (!require("openxlsx")) install.packages("openxlsx")
+
+require("openxlsx")
+
+#s2: read the original file on Excel. Clinical notes
+#s3: extra endpoionts. I don't think I will use them. 
+#s4: extra endpoionts notes. I don't think I will use them. 
+#s6: PH Assumption tests: I don't think i will need it 
+#s7: TSS info
+#s8: additional info
+
+tcga_clini <- read.xlsx("Data\\liu_s1_tcga_clinicalinformation.xlsx", sheet=1) 
+tcga_tests <- read.xlsx("Data\\liu_s1_tcga_clinicalinformation.xlsx", sheet=5)[,-c(7,8,9)] 
 
 
-#require(XML)
-require(FirebrowseR)
-require(ggplot2)
 
-#Workdiretory 
-setwd("~/GitHub/project_spring2019")
 
-##Reading Data
-cohorts = Metadata.Cohorts(format = "csv") # Download all available cohorts
-cancer.Type = cohorts[grep("breast", cohorts$description, ignore.case = T), 1]
-
-all.Received = F
-page.Counter = 1
-page.size = 150
-brca.Pats = list()
-while(all.Received == F){
-  brca.Pats[[page.Counter]] = Samples.Clinical(format = "csv",
-                                               cohort = cancer.Type,
-                                               page_size = page.size,
-                                               page = page.Counter)
-  if(page.Counter > 1)
-    colnames(brca.Pats[[page.Counter]]) = colnames(brca.Pats[[page.Counter-1]])
-  if(nrow(brca.Pats[[page.Counter]]) < page.size){
-    all.Received = T
-  } else{
-    page.Counter = page.Counter + 1
-  }
-}
-brca.Pats = do.call(rbind, brca.Pats)
-dim(brca.Pats)
-bd1 = brca.Pats
-
-#brca.Pats = brca.Pats[ which(brca.Pats$vital_status == "dead"), ]
-
-# Gene expression
-#Number of participants is too much, selecting only these genes
 # 
-# diff.Exp.Genes = c("ESR1", "GATA3", "XBP1", "FOXA1", "ERBB2", "GRB7", "EGFR","FOXC1", "MYC")
-# all.Found = F
+# if (!require("FirebrowseR")) devtools::install_github("mariodeng/FirebrowseR")
+# if (!require("ggplot2")) install.packages("ggplot2")
+# 
+# 
+# #require(XML)
+# require(FirebrowseR)
+# require(ggplot2)
+# 
+# 
+# ##Reading Data
+# cohorts = Metadata.Cohorts(format = "csv") # Download all available cohorts
+# cancer.Type = cohorts[grep("breast", cohorts$description, ignore.case = T), 1]
+# 
+# all.Received = F
 # page.Counter = 1
-# mRNA.Exp = list()
-# page.Size = 2000 # using a bigger page size is faster
-# while(all.Found == F){
-#   mRNA.Exp[[page.Counter]] = Samples.mRNASeq(format = "csv",
-#                                              gene = diff.Exp.Genes,
-#                                              cohort = "BRCA",
-#                                              tcga_participant_barcode =
-#                                                brca.Pats$tcga_participant_barcode[1:200],
-#                                              page_size = page.Size,
-#                                              page = page.Counter)
-#   if(nrow(mRNA.Exp[[page.Counter]]) < page.Size)
-#     all.Found = T
-#   else
+# page.size = 150
+# brca.Pats = list()
+# while(all.Received == F){
+#   brca.Pats[[page.Counter]] = Samples.Clinical(format = "csv",
+#                                                cohort = cancer.Type,
+#                                                page_size = page.size,
+#                                                page = page.Counter)
+#   if(page.Counter > 1)
+#     colnames(brca.Pats[[page.Counter]]) = colnames(brca.Pats[[page.Counter-1]])
+#   if(nrow(brca.Pats[[page.Counter]]) < page.size){
+#     all.Received = T
+#   } else{
 #     page.Counter = page.Counter + 1
+#   }
 # }
-# mRNA.Exp = do.call(rbind, mRNA.Exp)
-# dim(mRNA.Exp)
-# 
-# 
-# p = ggplot(mRNA.Exp, aes(factor(gene), z.score))
-# p +
-#   geom_boxplot(aes(fill = factor(sample_type))) +
-#   # we drop some outlier, so plot looks nicer, this also causes the warning
-#   scale_y_continuous(limits = c(-1, 5)) +
-#   scale_fill_discrete(name = "Tissue")
-# 
-
-
-#Data from cbioportal
-#http://www.cbioportal.org/rmatlab
-#LInk above does not work, but help('cgdsr') works
-if (!require("cgdsr")) install.packages("cgdsr")
-require(cgdsr)
-
-# Create CGDS object
-mycgds = CGDS("http://www.cbioportal.org/")
-
-# Test the CGDS endpoint URL using a few simple API tests
-test(mycgds) 
-
-# Get list of cancer studies at server
-getCancerStudies(mycgds)
-
-# Get available case lists (collection of samples) for a given cancer study  
-mycancerstudy = getCancerStudies(mycgds)[33,1]
-mycaselist = getCaseLists(mycgds,mycancerstudy)[1,1]
-
-# Get available genetic profiles
-mygeneticprofile = getGeneticProfiles(mycgds,mycancerstudy)[4,1]
-
-# Get data slices for a specified list of genes, genetic profile and case list
-getProfileData(mycgds,c('BRCA1','BRCA2'),mygeneticprofile,mycaselist)
-
-# Get clinical data for the case list
-myclinicaldata = getClinicalData(mycgds,mycaselist)
-bd2 = myclinicaldata
-
-
-
-#checking the barcodes 
-id1 = as.character(bd1[,1])
-id2 = as.character(row.names(bd2))
-
-id2 = gsub(".01", "", id2)
-id2 = gsub(".", "-", id2, fixed = T)
-
-id1 = id1[order(id1)]
-id2 = id2[order(id2)]
-
-head(id1); head(id2)
-id1_2 = setdiff(id1, id2)
-
-bd1$id = 0
-for(i in 1:length(id1_2)){
-  bd1$id[bd1$tcga_participant_barcode==id1_2[i]]=1
-}
-bd1_2 = subset(bd1, id==1)
-dim(bd1_2); length(id1_2)
+# brca.Pats = do.call(rbind, brca.Pats)
+# dim(brca.Pats)
+# bd1 = brca.Pats
