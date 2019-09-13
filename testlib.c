fbe894a0fc7902f027a6a4ea3e084bab2abeb375
Raquel Aoki <23690490+raquelaoki@users.noreply.github.com>
2019-09-13 12:13:56 -0700

    organazing code

---
+++ testlib.c
@@ -0,0 +1,26 @@
+//#include <stdio.h>
+
+//int main()
+//{
+//    printf("Hello, World! This is a native C program compiled on the command line.\n");
+ //   return 0;
+//}
+
+/* C program to print Hello World! */
+ 
+#include <stdio.h>
+
+// function to print Hello World!
+void printMessage(void)
+{
+    printf("Hello World!");
+}
+ 
+int main()
+{
+     
+    //calling function
+    printMessage();
+ 
+    return 0;
+}
---
+++ testlibwrapper.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 13 11:47:29 2019
+
+@author: raoki
+"""
+
+import ctypes
+
+ptestlib.myprint()
