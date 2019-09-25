fb0278268d8d3cbf3a78a6f8837f895993dbf290
Raquel Aoki <23690490+raquelaoki@users.noreply.github.com>
2019-09-25 12:59:15 -0700

    experiments

--- script_v007_baselines.py
+++ script_v007_baselines.py
@@ -1,11 +1,12 @@
 '''Loading libraries'''
 import pandas as pd 
 import numpy as np 
-import time
+#import time
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
-from scipy.stats import gamma
-import copy
+from sklearn.metrics import f1_score
+#from scipy.stats import gamma
+#import copy
 
 '''
 Notes:
@@ -15,85 +16,101 @@ Notes:
 
 
 
-
 '''Hyperparameters'''
-k = 100 #Latents Dimension 
-sim = 600 #Simulations 
-bach_size = 200 #Batch size for memory purposes 
-step1 = 10 #Saving chain every step1 steps 
-step2 = 20
+k = 30#Latents Dimension 
+#sim = 600 #Simulations 
+#bach_size = 200 #Batch size for memory purposes 
+#step1 = 10 #Saving chain every step1 steps 
+#step2 = 20
 
 #WRONG< UPDATE HERE 
-if bach_size//step2 <= 20:
-    print('ERROR ON MCMC, this division must be bigger than 20')
+#if bach_size//step2 <= 20:
+#    print('ERROR ON MCMC, this division must be bigger than 20')
 
 '''Loading dataset'''
 #filename = "C:\\Users\\raoki\\Documents\\GitHub\\project_spring2019\\DataNew\\tcga_train_filtered.txt"
 #filename = "C:\\Users\\raoki\\Documents\\GitHub\\project_spring2019\\DataNew\\tcga_train_binary.txt"
-#filename = "C:\\Users\\raoki\\Documents\\GitHub\\project_spring2019\\DataNew\\tcga_train_gexpression.txt"
+filename = "C:\\Users\\raoki\\Documents\\GitHub\\project_spring2019\\DataNew\\tcga_train_gexpression.txt"
 #filename = "C:\\Users\\raoki\\Documents\\GitHub\\project_spring2019\\DataNew\\tcga_train_ge_balanced.txt"
-filename = "C:\\Users\\raque\\Documents\\GitHub\\project_spring2019\\Data\\tcga_train_gexpression.txt"
+#filename = "C:\\Users\\raque\\Documents\\GitHub\\project_spring2019\\Data\\tcga_train_gexpression.txt"
 data = pd.read_csv(filename, sep=';')
 
 
-'''Splitting Dataset'''
-train, test = train_test_split(data, test_size=0.3, random_state=22)
-#train = data
+f1_sample = []
+acc_sample = []
 
-'''Organizing columns names'''
-remove = train.columns[[0,1]]
-y = train.columns[1]
-y01 = np.array(train[y])
-train = train.drop(remove, axis = 1)
-y01_t = np.array(test[y])
-test = test.drop(remove, axis = 1)
+for sample in np.arange(0,100):
+    '''Splitting Dataset'''
+    train, test = train_test_split(data, test_size=0.3) #random_state=22
+    #train = data
+    
+    '''Organizing columns names'''
+    remove = train.columns[[0,1]]
+    y = train.columns[1]
+    y01 = np.array(train[y])
+    train = train.drop(remove, axis = 1)
+    y01_t = np.array(test[y])
+    test = test.drop(remove, axis = 1)
+    
+    
+    '''Defining variables'''
+    v = train.shape[1] #genes
+    j = train.shape[0] #patients 
+    
+    
+    '''Matrix Factorization''' 
+    from sklearn.decomposition import NMF
+    model1 = NMF(n_components=k, init='random') #random_state=0
+    latent1 = model1.fit_transform(train)
+    latent1_t= model1.transform(test)
+    
+    '''Logistc Regression''' 
+    from sklearn.linear_model import LogisticRegressionCV
+    model6 = LogisticRegressionCV(Cs=6,penalty='l2',fit_intercept=True).fit(latent1,y01)
+    pred6 = model6.predict(latent1)
+    pred6_t = model6.predict(latent1_t)
+    
+    print(confusion_matrix(pred6,y01))
+    ac = confusion_matrix(pred6_t,y01_t)
+    print('testing set')
+    print((ac[0,0]+ac[1,1])/ac.sum())
+    f1_sample.append(f1_score(y01_t,pred6_t))
+    acc_sample.append((ac[0,0]+ac[1,1])/ac.sum())
+    #with k=100, tcga_train_geexpression, acc on testing set is 0.73 and training set is 0.79
 
 
-'''Defining variables'''
-v = train.shape[1] #genes
-j = train.shape[0] #patients 
 
+with open('baselines_f1.txt', 'w') as f:
+    for item in f1_sample:
+        f.write("%s\n" % item)
 
-'''Matrix Factorization''' 
-from sklearn.decomposition import NMF
-model1 = NMF(n_components=100, init='random', random_state=0)
-latent1 = model1.fit_transform(train)
-latent1_t= model1.transform(test)
+with open('baselines_acc.txt', 'w') as f:
+    for item in acc_sample:
+        f.write("%s\n" % item)
 
-'''Logistc Regression''' 
-from sklearn.linear_model import LogisticRegressionCV
-model6 = LogisticRegressionCV(Cs=6,penalty='l2',fit_intercept=True).fit(latent1,y01)
-pred6 = model6.predict(latent1)
-pred6_t = model6.predict(latent1_t)
-
-confusion_matrix(pred6,y01)
-confusion_matrix(pred6_t,y01_t)
-
-#with k=100, tcga_train_geexpression, acc on testing set is 0.73 and training set is 0.79
-
-
-
-
+print('min, mean, max and var \n')
+print('F1: ',f1_sample.min(), f1_sample.mean(),f1_sample.max() ,f1_sample.var(),'\n')
+print('Acc: ',acc_sample.min(), acc_sample.mean(),acc_sample.max() ,acc_sample.var())
 
 '''Auto-enconder''' 
 #https://github.com/greenelab/tybalt
-'''PCA''' 
+'''PCA
 from sklearn.decomposition import PCA
 model3 = PCA(n_components = 100)
 latent3 = model3.fit_transform(train)
-
+''' 
 '''Random Forest''' 
 #mod4 
 
 '''Neural Networks''' 
 #m5 
 
-'''Logistc Regression''' 
+'''Logistc Regression
 from sklearn.linear_model import LogisticRegressionCV
 model6 = LogisticRegressionCV(Cs=6,penalty='l2',fit_intercept=True).fit(latent1,y01)
 pred6 = model6.predict(latent1)
-
-'''Naive Bayes''' 
+''' 
+'''Naive Bayes
 from sklearn.naive_bayes import GaussianNB
 model7 = GaussianNB()
-pred7 = model7.fit(latent1, y01).predict(latent1)
+pred7 = model7.fit(latent1, y01).predict(latent1)''' 
--- script_v010.py
+++ script_v010.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np 
 import time
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix,f1_score
 from scipy.stats import gamma
 import copy
 #import sklearn as sk
@@ -41,26 +41,8 @@ filename = "C:\\Users\\raoki\\Documents\\GitHub\\project_spring2019\\DataNew\\tc
 #filename = "C:\\Users\\raoki\\Documents\\GitHub\\project_spring2019\\DataNew\\tcga_train_ge_balanced.txt"
    
 data = pd.read_csv(filename, sep=';')
+
 #data = data.iloc[:, 0:300]
-
-'''Splitting Dataset'''
-train, test = train_test_split(data, test_size=0.3) #random_state=22
-#train = data
-
-'''Organizing columns names'''
-remove = train.columns[[0,1]]
-y = train.columns[1]
-y01 = np.array(train[y])
-train = train.drop(remove, axis = 1)
-y01_t = np.array(test[y])
-test = test.drop(remove, axis = 1)
-train0 = np.matrix(train)
-
-
-'''Defining variables'''
-v = train.shape[1] #genes
-j = train.shape[0] #patients 
-
 '''Parameters'''
 class parameters:
     __slots__ = ( 'la_cj','la_sk','la_ev','lm_phi','lm_tht')   
@@ -72,18 +54,9 @@ class parameters:
         self.lm_tht = latent_tht #string of matrix  (jk) in array format 
 
 
-'''Initial Values'''
-current = parameters(np.repeat(0.5,j), #la_cj 0.25
-                   np.repeat(150.5,k*2).reshape(2,k), #la_sk 62
-                   np.repeat(1.0004,v), #la_ev FIXED
-                   np.repeat(1/v,v*k).reshape(v,k),#lm_phi v x k 
-                   np.repeat(150.5,j*k).reshape(j,k)) #lm_theta k x j
-                   #np.repeat(0.5, j), #la_pj
-                   #np.repeat(0.5,k)) #la_qk 
 
 
 '''Gibbs Sampling'''
-
 def gibbs(current,train0,j,v,k,y01):
     new = copy.deepcopy(current) 
     lvjk = np.zeros((v,j,k))
@@ -119,7 +92,6 @@ def gibbs(current,train0,j,v,k,y01):
     return(new)
 
 '''Accuracy'''
-
 def acc(theta,sk1,cj,y):
     y0 = gamma.logpdf(x=theta,a = sk1[0,:],scale = 1)
     y1 = gamma.logpdf(x=theta,a = sk1[1,:],scale = 1)
@@ -127,117 +99,168 @@ def acc(theta,sk1,cj,y):
     y3 = y3.sum(axis=1)
     y3[y3<=0] = 0
     y3[y3>0] = 1
+    #print(f1_score(y,y3))
     return confusion_matrix(y,y3)
+
+'''Accuracy'''
+def PGM_pred(theta,sk1,cj,y):
+    y0 = gamma.logpdf(x=theta,a = sk1[0,:],scale = 1)
+    y1 = gamma.logpdf(x=theta,a = sk1[1,:],scale = 1)
+    y3 = y1-y0
+    y3 = y3.sum(axis=1)
+    y3[y3<=0] = 0
+    y3[y3>0] = 1
+    #print(f1_score(y,y3))
+    return y3
+
+#LOOP
+
+
+f1_sample = []
+acc_sample = []    
+for experiment in np.arange(0,100):  
+    print('Experiment ', experiment, ' of 100')
+    '''Splitting Dataset'''
+    train, test = train_test_split(data, test_size=0.3) #random_state=22
+    #train = data
     
-
-'''Creating the chains'''
-chain_la_sk = np.tile(current.la_sk.reshape(-1,1),(1,int(bach_size/step1)))
-chain_la_cj = np.tile(current.la_cj.reshape(-1,1),(1,int(bach_size/step1)))
-chain_lm_tht = np.tile(current.lm_tht.reshape(-1,1),(1,int(bach_size/step1)))
-chain_lm_phi = np.tile(current.lm_phi.reshape(-1,1),(1,int(bach_size/step1)))
-
-'''Sampling'''
-start_time = time.time()    
+    '''Organizing columns names'''
+    remove = train.columns[[0,1]]
+    y = train.columns[1]
+    y01 = np.array(train[y])
+    train = train.drop(remove, axis = 1)
+    y01_t = np.array(test[y])
+    test = test.drop(remove, axis = 1)
+    train0 = np.matrix(train)
     
-for ite in np.arange(0,sim//bach_size):    
-    count_s1 = 0
-    count_s2 = 0
-    chain_la_sk[:,count_s1]=current.la_sk.reshape(1,-1)
-    chain_la_cj[:,count_s1]=current.la_cj.reshape(1,-1)
-    chain_lm_tht[:,count_s1]=current.lm_tht.reshape(1,-1)
-    chain_lm_phi[:,count_s1]=current.lm_phi.reshape(1,-1)
-    print('iteration--',ite,' of ',sim//bach_size)   
-    #.print('it should be 981',data.shape)       
-    for i in np.arange(1,bach_size):
-        new  = gibbs(current,train0,j,v,k,y01)
-        '''Updating chain'''
-        if i%10==0:
-            print('------------', i, ' of ',bach_size) 
-            count_s1+=1
-            chain_la_sk[:,count_s1]=new.la_sk.reshape(1,-1)
-            chain_la_cj[:,count_s1]=new.la_cj.reshape(1,-1)
-            chain_lm_tht[:,count_s1]=new.lm_tht.reshape(1,-1)
-            chain_lm_phi[:,count_s1]=new.lm_phi.reshape(1,-1)
-            if i%90 == 0: 
-                #print('iteration ',ite, 'bach ', i) 
-                print(acc(current.lm_tht,current.la_sk,current.la_cj, y01))
-                test1 = np.dot(current.lm_tht,np.transpose(current.lm_phi))
-                print(test1.mean(), train0.mean())
-        #print('tht',current.lm_tht.mean(),new.lm_tht.mean())
-        #print('phi',current.lm_phi[0:10,1],new.lm_phi[0:10,1])
-        #print('sk',current.la_sk.mean(),new.la_sk.mean())
-        #print('cj',current.la_cj.mean(),new.la_cj.mean())
-        #print('\n')
-
-        current= copy.deepcopy(new )  
-    np.savetxt('Data\\output_lask_id'+str(id)+'_bach'+str(ite)+'.txt', chain_la_sk, delimiter=',',fmt='%5s')
-    np.savetxt('Data\\output_lacj_id'+str(id)+'_bach'+str(ite)+'.txt', chain_la_cj, delimiter=',',fmt='%5s')
-    np.savetxt('Data\\output_lmtht_id'+str(id)+'_bach'+str(ite)+'.txt', chain_lm_tht, delimiter=',',fmt='%5s')
-    np.savetxt('Data\\output_lmphi_id'+str(id)+'_bach'+str(ite)+'.txt', chain_lm_phi, delimiter=',',fmt='%5s')
-
-
-print("--- %s min ---" % int((time.time() - start_time)/60))
-print("--- %s hours ---" % int((time.time() - start_time)/(60*60)))
-
-
-'''Loading average values back for predictions'''
-ite0 = 2
-la_sk = np.loadtxt('C:\\Users\\raoki\\Documents\\GitHub\\project_spring2019\\Data\\output_lask_id'+str(id)+'_bach'+str(ite0)+'.txt', delimiter=',').mean(axis=1)
-la_cj = np.loadtxt('C:\\Users\\raoki\\Documents\\GitHub\\project_spring2019\\Data\\output_lacj_id'+str(id)+'_bach'+str(ite0)+'.txt', delimiter=',').mean(axis=1)
-lm_phi = np.loadtxt('C:\\Users\\raoki\\Documents\\GitHub\\project_spring2019\\Data\\output_lmphi_id'+str(id)+'_bach'+str(ite0)+'.txt', delimiter=',').mean(axis=1)
-lm_tht = np.loadtxt('C:\\Users\\raoki\\Documents\\GitHub\\project_spring2019\\Data\\output_lmtht_id'+str(id)+'_bach'+str(ite0)+'.txt', delimiter=',').mean(axis=1)
-
-
-for ite in np.arange(ite0+1,sim//bach_size):
-    la_sk = la_sk + np.loadtxt('C:\\Users\\raoki\\Documents\\GitHub\\project_spring2019\\Data\\output_lask_id'+str(id)+'_bach'+str(ite)+'.txt', delimiter=',').mean(axis=1)
-    la_cj = la_cj + np.loadtxt('C:\\Users\\raoki\\Documents\\GitHub\\project_spring2019\\Data\\output_lacj_id'+str(id)+'_bach'+str(ite)+'.txt', delimiter=',').mean(axis=1)
-    lm_phi = lm_phi + np.loadtxt('C:\\Users\\raoki\\Documents\\GitHub\\project_spring2019\\Data\\output_lmphi_id'+str(id)+'_bach'+str(ite)+'.txt', delimiter=',').mean(axis=1)
-    lm_tht = lm_tht + np.loadtxt('C:\\Users\\raoki\\Documents\\GitHub\\project_spring2019\\Data\\output_lmtht_id'+str(id)+'_bach'+str(ite)+'.txt', delimiter=',').mean(axis=1)
-
-la_sk = la_sk/((sim//bach_size)-1)
-la_cj = la_cj/((sim//bach_size)-1)
-lm_phi = lm_phi/((sim//bach_size)-1)
-lm_tht = lm_tht/((sim//bach_size)-1)
-
-
-la_sk = la_sk.reshape(2,k)
-la_cj = la_cj.reshape(j,1)
-lm_tht = lm_tht.reshape(j,k)
-lm_phi = lm_phi.reshape(v,k)
-
-print(la_cj.shape, la_sk.shape,la_sk[0,:].shape ,lm_tht.shape)
-print(current.la_cj.shape, current.la_sk.shape,current.la_sk[0,:].shape ,current.lm_tht.shape)
-
-print('final accucary on the average values sampled')
-acc(lm_tht,la_sk,la_cj,y01)
-#acc(current.lm_tht,current.la_sk,current.la_cj,y01)
-
-
-'''Predictions on testing set'''
-lm_tht_pred = np.repeat(0.5,test.shape[0]*k).reshape(test.shape[0],k)
-test0 = np.matrix(test) 
-
-best_nb = []
-for i in range(2,12):    
+    
+    '''Defining variables'''
+    v = train.shape[1] #genes
+    j = train.shape[0] #patients 
+    
+    
+    '''Initial Values'''
+    current = parameters(np.repeat(0.5,j), #la_cj 0.25
+                       np.repeat(150.5,k*2).reshape(2,k), #la_sk 62
+                       np.repeat(1.0004,v), #la_ev FIXED
+                       np.repeat(1/v,v*k).reshape(v,k),#lm_phi v x k 
+                       np.repeat(150.5,j*k).reshape(j,k)) #lm_theta k x j
+                       #np.repeat(0.5, j), #la_pj
+                       #np.repeat(0.5,k)) #la_qk 
+     
+    
+    '''Creating the chains'''
+    chain_la_sk = np.tile(current.la_sk.reshape(-1,1),(1,int(bach_size/step1)))
+    chain_la_cj = np.tile(current.la_cj.reshape(-1,1),(1,int(bach_size/step1)))
+    chain_lm_tht = np.tile(current.lm_tht.reshape(-1,1),(1,int(bach_size/step1)))
+    chain_lm_phi = np.tile(current.lm_phi.reshape(-1,1),(1,int(bach_size/step1)))
+    
+    '''Sampling'''
+    start_time = time.time()    
+        
+    for ite in np.arange(0,sim//bach_size):    
+        count_s1 = 0
+        count_s2 = 0
+        chain_la_sk[:,count_s1]=current.la_sk.reshape(1,-1)
+        chain_la_cj[:,count_s1]=current.la_cj.reshape(1,-1)
+        chain_lm_tht[:,count_s1]=current.lm_tht.reshape(1,-1)
+        chain_lm_phi[:,count_s1]=current.lm_phi.reshape(1,-1)
+        print('iteration--',ite,' of ',sim//bach_size)   
+        #.print('it should be 981',data.shape)       
+        for i in np.arange(1,bach_size):
+            new  = gibbs(current,train0,j,v,k,y01)
+            '''Updating chain'''
+            if i%10==0:
+                #print('------------', i, ' of ',bach_size) 
+                count_s1+=1
+                chain_la_sk[:,count_s1]=new.la_sk.reshape(1,-1)
+                chain_la_cj[:,count_s1]=new.la_cj.reshape(1,-1)
+                chain_lm_tht[:,count_s1]=new.lm_tht.reshape(1,-1)
+                chain_lm_phi[:,count_s1]=new.lm_phi.reshape(1,-1)
+                if i%90 == 0: 
+                    #print('iteration ',ite, 'bach ', i) 
+                    #print(acc(current.lm_tht,current.la_sk,current.la_cj, y01))
+                    test1 = np.dot(current.lm_tht,np.transpose(current.lm_phi))
+                    print(test1.mean(), train0.mean())
+            #print('tht',current.lm_tht.mean(),new.lm_tht.mean())
+            #print('phi',current.lm_phi[0:10,1],new.lm_phi[0:10,1])
+            #print('sk',current.la_sk.mean(),new.la_sk.mean())
+            #print('cj',current.la_cj.mean(),new.la_cj.mean())
+            #print('\n')
+    
+            current= copy.deepcopy(new )  
+        np.savetxt('Data\\output_lask_id'+str(id)+'_bach'+str(ite)+'.txt', chain_la_sk, delimiter=',',fmt='%5s')
+        np.savetxt('Data\\output_lacj_id'+str(id)+'_bach'+str(ite)+'.txt', chain_la_cj, delimiter=',',fmt='%5s')
+        np.savetxt('Data\\output_lmtht_id'+str(id)+'_bach'+str(ite)+'.txt', chain_lm_tht, delimiter=',',fmt='%5s')
+        np.savetxt('Data\\output_lmphi_id'+str(id)+'_bach'+str(ite)+'.txt', chain_lm_phi, delimiter=',',fmt='%5s')
+    
+    
+        print("--- %s min ---" % int((time.time() - start_time)/60))
+        print("--- %s hours ---" % int((time.time() - start_time)/(60*60)))
+    
+    
+    '''Loading average values back for predictions'''
+    ite0 = 2
+    la_sk = np.loadtxt('C:\\Users\\raoki\\Documents\\GitHub\\project_spring2019\\Data\\output_lask_id'+str(id)+'_bach'+str(ite0)+'.txt', delimiter=',').mean(axis=1)
+    la_cj = np.loadtxt('C:\\Users\\raoki\\Documents\\GitHub\\project_spring2019\\Data\\output_lacj_id'+str(id)+'_bach'+str(ite0)+'.txt', delimiter=',').mean(axis=1)
+    lm_phi = np.loadtxt('C:\\Users\\raoki\\Documents\\GitHub\\project_spring2019\\Data\\output_lmphi_id'+str(id)+'_bach'+str(ite0)+'.txt', delimiter=',').mean(axis=1)
+    lm_tht = np.loadtxt('C:\\Users\\raoki\\Documents\\GitHub\\project_spring2019\\Data\\output_lmtht_id'+str(id)+'_bach'+str(ite0)+'.txt', delimiter=',').mean(axis=1)
+    
+    
+    for ite in np.arange(ite0+1,sim//bach_size):
+        la_sk = la_sk + np.loadtxt('C:\\Users\\raoki\\Documents\\GitHub\\project_spring2019\\Data\\output_lask_id'+str(id)+'_bach'+str(ite)+'.txt', delimiter=',').mean(axis=1)
+        la_cj = la_cj + np.loadtxt('C:\\Users\\raoki\\Documents\\GitHub\\project_spring2019\\Data\\output_lacj_id'+str(id)+'_bach'+str(ite)+'.txt', delimiter=',').mean(axis=1)
+        lm_phi = lm_phi + np.loadtxt('C:\\Users\\raoki\\Documents\\GitHub\\project_spring2019\\Data\\output_lmphi_id'+str(id)+'_bach'+str(ite)+'.txt', delimiter=',').mean(axis=1)
+        lm_tht = lm_tht + np.loadtxt('C:\\Users\\raoki\\Documents\\GitHub\\project_spring2019\\Data\\output_lmtht_id'+str(id)+'_bach'+str(ite)+'.txt', delimiter=',').mean(axis=1)
+    
+    la_sk = la_sk/((sim//bach_size)-1)
+    la_cj = la_cj/((sim//bach_size)-1)
+    lm_phi = lm_phi/((sim//bach_size)-1)
+    lm_tht = lm_tht/((sim//bach_size)-1)
+    
+    
+    la_sk = la_sk.reshape(2,k)
+    la_cj = la_cj.reshape(j,1)
+    lm_tht = lm_tht.reshape(j,k)
+    lm_phi = lm_phi.reshape(v,k)
+    
+    #print(la_cj.shape, la_sk.shape,la_sk[0,:].shape ,lm_tht.shape)
+    #print(current.la_cj.shape, current.la_sk.shape,current.la_sk[0,:].shape ,current.lm_tht.shape)
+    
+    #print('final accucary on the average values sampled')
+    #acc(lm_tht,la_sk,la_cj,y01)
+    #acc(current.lm_tht,current.la_sk,current.la_cj,y01)
+    
+    '''Predictions on testing set'''
+    lm_tht_pred = np.repeat(0.5,test.shape[0]*k).reshape(test.shape[0],k)
+    test0 = np.matrix(test) 
+    
     for j in np.arange(test.shape[0]):
         # intialise data of lists. 
         sim_list = list(cosine_similarity(test0[j,:], train0)[0])
         sim_list= pd.DataFrame({'sim':sim_list})
         sim_list = sim_list.sort_values(by=['sim'],  ascending=False)
-        lm_tht_pred[j,:] = lm_tht[list(sim_list.index[0:i])].mean(axis=0)         
-
-    ac = acc(lm_tht_pred,la_sk,la_cj,y01_t)
-    best_nb.append([i,(ac[0,0]+ac[1,1])/ac.sum()])
+        lm_tht_pred[j,:] = lm_tht[list(sim_list.index[0:6])].mean(axis=0)         
     
-print('acc using similarity among patients')    
-#print('ac using similarity',(ac[0,0]+ac[1,1])/ac.sum())
-print(best_nb)
+        y01_t_p = PGM_pred(lm_tht_pred,la_sk,la_cj,y01_t)
+        ac = confusion_matrix(y01_t, y01_t_p)
+        
+        acc_sample.append((ac[0,0]+ac[1,1])/ac.sum())    
+        f1_sample.append(f1_score(y01_t, y01_t_p))
+        
 
 
-best_nb
+with open('pgm_id12_f1.txt', 'w') as f:
+    for item in f1_sample:
+        f.write("%s\n" % item)
+
+with open('pgm_id12_acc.txt', 'w') as f:
+    for item in acc_sample:
+        f.write("%s\n" % item)
 
 
-
+print('acc: ',acc_sample)
+print('f1 : ', f1_sample)
 
 
 
