5cb28b21e3805b60a57a40674deeb5606b6be4cb
Raquel Aoki <23690490+raquelaoki@users.noreply.github.com>
2019-11-18 16:16:00 -0800

    Outcome Model
    Implementing new outcome model approach

--- scr/main.py
+++ scr/main.py
@@ -14,12 +14,11 @@ pd.set_option('display.max_columns', 500)
 '''
 Flags
 '''
-RUN_MCMC = False
+RUN_MCMC = True
 RUN_OUTCOME = False
 RUN_PREDICTIONS = False
 RUN_LOAD_MCMC = False
-RUN_PREDICTIVE_CHECK = False
-RUN_MF = True
+RUN_MF = False
 '''
 Note:
     - model id = '12' on MLCB
@@ -35,7 +34,8 @@ test
 '''
 
 '''MCMC Hyperparameters'''
-k = 30 #Latents Dimension
+k_mf = 20 #Latents Dimension
+k_mcmc = 10
 sim = 2000 #Simulations
 bach_size = 200 #Batch size for memory purposes
 step1 = 10 #Saving chain every step1 steps
@@ -61,25 +61,30 @@ class parameters:
 data = pd.read_csv(filename, sep=';')
 #data = data.iloc[0:500, 0:100]
 train, train_validation, j, v, y01, abr, hold_mask = fc.holdout(data, 0.001)
-fc.mcmc(train,y01,sim,bach_size,step1,k,id,RUN_MCMC)
-la_sk,la_cj,lm_tht,lm_phi = fc.load_chain(id,sim,bach_size,j,v,k,RUN_LOAD_MCMC)
-#if RUN_LOAD_MCMC:
-    #print('it shoul be 0.5: ')
-    #p = fc.predictive_check_mcmc(train, train_validation,lm_tht, lm_phi, la_sk, y01,hold_mask, RUN_PREDICTIVE_CHECK)
-    #print(np.mean(p))
-
-W, F = fc.matrixfactorization(train,k,RUN_MF)
-if RUN_MF:
-    #Time consuming
-    v_pred, test_result = fc.predictive_check_new(train,W.dot(F),RUN_MF)
+fc.mcmc(train,y01,sim,bach_size,step1,k_mcmc,id,RUN_MCMC)
+la_sk,la_cj,lm_tht,lm_phi = fc.load_chain(id,sim,bach_size,j,v,k_mcmc,RUN_LOAD_MCMC)
+if RUN_LOAD_MCMC:
+    Z = lm_tht.dot(np.transpose(lm_phi))
+    v_pred, test_result = fc.predictive_check_new(train,Z,True)
     if(test_result):
         print('Predictive Check test: PASS')
-    else: 
+        fc.outcome_model(train,lm_tht,y01)
+    else:
+        print('Predictive Check Test: FAIL')
+
+
+W, F = fc.matrixfactorization(train,k_mf,RUN_MF)
+if RUN_MF:
+    #Time consuming
+    v_pred, test_result = fc.predictive_check_new(train,W.dot(F),True)
+    if(test_result):
+        print('Predictive Check test: PASS')
+    else:
         print('Predictive Check Test: FAIL')
 
 #Z = W.dot(F)
 #X = train
-#i = 0 
+#i = 0
 #Z_train, Z_test, X_train, X_test = train_test_split(Z, , test_size=0.2)
 
 
@@ -102,11 +107,11 @@ if RUN_MF:
 
 
     #'''Outcome Model'''
-    #c,f,cm = fc.OutcomeModel('rf',train0,lm_tht,y01)
-    #print('\n Metrics OutcomeModel: ','\n',f,'\n',cm,'\n')
+    #c,f,cm = fc.outcome_model('rf',train0,lm_tht,y01)
+    #print('\n Metrics outcome_model: ','\n',f,'\n',cm,'\n')
 
-    #c,f,cm = fc.OutcomeModel('lr',train0,lm_tht,y01)
-    #print('\n Metrics OutcomeModel: ','\n',f,'\n',cm,'\n')
+    #c,f,cm = fc.outcome_model('lr',train0,lm_tht,y01)
+    #print('\n Metrics outcome_model: ','\n',f,'\n',cm,'\n')
 
 
     #'''Driver Genes'''
@@ -132,8 +137,8 @@ if RUN_MF:
     #gene_coef_sub = pd.merge(gene_coef_sub,cgc_list, left_on = 'genes',right_on='Gene Symbol')
 
     #print(gene_coef_sub.shape)#
- 
-    
+
+
     #   return v1,v2
 
 
